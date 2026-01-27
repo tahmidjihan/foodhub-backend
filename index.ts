6d19249eb045ad73027aab69503b2ccd4f5aba8f
Tahmid Jihan <tahmidjihan321@gmail.com>
2026-01-28 00:08:23 +0600

    schema

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
