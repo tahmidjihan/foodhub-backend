54050fe19f69492aa99cfe826d91a6f91769bed2
Tahmid Jihan <tahmidjihan321@gmail.com>
2026-01-27 15:30:01 +0600

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
